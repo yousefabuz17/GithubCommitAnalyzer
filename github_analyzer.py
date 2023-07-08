fb834f413c6ad57183e4b0bf52852d65bdca7852
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-07 23:34:30 -0400

    Retrieved and saved as csv files for project/daily

--- github_analyzer.py
+++ github_analyzer.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import os
 from datetime import datetime as dt
 from rich.console import Console
 from pathlib import Path
@@ -8,21 +9,34 @@ from bs4 import BeautifulSoup
 
 
 class GithubCommit:
-    def __init__(self, user_name, projects=None):
+    def __init__(self, user_name, projects: list[str]=None):
         self.user_name = user_name
-        self.project = projects
+        self.projects = projects
         self.url = 'https://github.com/{}/'.format(self.user_name)
-        self.project_url = self.url + self.project if self.project else self.url
     
     def projects_parse_url(self):
         data = []
-        response = requests.get(self.project_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for i in soup.find_all('div'):
-            for j in i.find_all(class_="application-main"):
-                if j.text is None:
-                    continue
-                j = j.text.split()
+        for project in self.projects:
+            response = requests.get(self.url+project)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for i in soup.find_all('div'):
+                for j in i.find_all(class_="application-main"):
+                    if j.text is None:
+                        continue
+                    j = j.text.split()
+                    project_names = j[2]
+                    commits_index = [m-1 for m,_ in enumerate(j) if _=='commits'][0]
+                    commits_num = int(j[commits_index:commits_index + 2][0])
+                    data.append((project_names, commits_num))
+        return self.projects_data_to_csv(data, 'github_projects_data.csv')
+    
+    def projects_data_to_csv(self, data, file_name):
+        with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Project Name', 'Total Project Commit Count'])
+            for i in data:
+                writer.writerow(i)
+        return f'GitHub Projects Data saved as \'{file_name}\''
     
     def daily_parse_url(self):
         data = []
@@ -35,20 +49,20 @@ class GithubCommit:
                 j = j.text.split()
                 data.append(([i if i.isdigit() else 0 for i in j[:3]],j[-3:]))
         updated_data = list(filter(lambda x: str(x)!='([], [])', data))
-        return self.data_to_csv(updated_data, 'github_daily_data.csv')
+        return self.daily_data_to_csv(updated_data, 'github_daily_data.csv')
     
-    def data_to_csv(self, data, file_name):
+    def daily_data_to_csv(self, data, file_name):
         with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(['Date', 'Commit Count'])
             for i in data:
                 writer.writerow([' '.join(i[-1]), i[0][0]])
-        return f'Data saved as \'{file_name}\''
+        return f'Daily GitHub Data saved as \'{file_name}\''
     
 def main():
-    github = GithubCommit('yousefabuz17')
+    projects = list(filter(lambda x: x.isalpha(), os.listdir()))
+    github = GithubCommit('yousefabuz17', projects)
     github_daily = GithubCommit('yousefabuz17').daily_parse_url()
     github_project = github.projects_parse_url()
-    print(github_project)
 main()
 
