937ba0fa784e5a2359e8a5d767be990a8c5e6264
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-08 01:47:39 -0400

    Implemented Exception handling.

--- github_analyzer.py
+++ github_analyzer.py
@@ -3,49 +3,57 @@ import csv
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime as dt
+import logging
+from rich.console import Console
 from pathlib import Path
 from bs4 import BeautifulSoup
 from aiohttp import ClientSession
 from matplotlib.ticker import MaxNLocator
 
-
+console = Console()
 class GithubCommit:
     def __init__(self, user_name, projects: list[str] = None):
         self.user_name = user_name
         self.projects = projects
         self.url = 'https://github.com/{}/'.format(self.user_name)
+        console.print('\tGithub Commit Analyzer', style='bold green')
 
     async def projects_parse_url(self, session):
         data = []
-        for project in self.projects:
-            async with session.get(self.url + project) as response:
-                html = await response.text()
-                soup = BeautifulSoup(html, 'html.parser')
-                for i in soup.find_all('div'):
-                    for j in i.find_all(class_="application-main"):
-                        if j.text is None:
-                            continue
-                        j = j.text.split()
-                        project_names = j[2]
-                        commits_index = [m - 1 for m, _ in enumerate(j) if _ == 'commits'][0]
-                        commits_num = int(j[commits_index:commits_index + 2][0])
-                        data.append((project_names, commits_num))
-        return DataToCSV(data, 'github_projects_data.csv', file_type='projects')
+        try:
+            for project in self.projects:
+                async with session.get(self.url + project) as response:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, 'html.parser')
+                    for i in soup.find_all('div'):
+                        for j in i.find_all(class_="application-main"):
+                            if j.text is None:
+                                continue
+                            j = j.text.split()
+                            project_names = j[2]
+                            commits_index = [m - 1 for m, _ in enumerate(j) if _ == 'commits'][0]
+                            commits_num = int(j[commits_index:commits_index + 2][0])
+                            data.append((project_names, commits_num))
+            return DataToCSV(data, 'github_projects_data.csv', file_type='projects')
+        except (ValueError, IndexError, ConnectionError) as e:
+            print(f"Error occurred during project parsing: {str(e)}")
 
     async def daily_parse_url(self, session):
         data = []
-        async with session.get(self.url) as response:
-            html = await response.text()
-            soup = BeautifulSoup(html, 'html.parser')
-            for i in soup.find_all('main'):
-                for j in i.find_all(class_="ContributionCalendar-day"):
-                    if j.text is None:
-                        continue
-                    j = j.text.split()
-                    data.append(([i if i.isdigit() else 0 for i in j[:3]], j[-3:]))
-        filtered_data = list(filter(lambda x: str(x) != '([], [])', data))
-        return DataToCSV(filtered_data, 'github_daily_data.csv', file_type='daily')
+        try:
+            async with session.get(self.url) as response:
+                html = await response.text()
+                soup = BeautifulSoup(html, 'html.parser')
+                for i in soup.find_all('main'):
+                    for j in i.find_all(class_="ContributionCalendar-day"):
+                        if j.text is None:
+                            continue
+                        j = j.text.split()
+                        data.append(([i if i.isdigit() else 0 for i in j[:3]], j[-3:]))
+            filtered_data = list(filter(lambda x: str(x) != '([], [])', data))
+            return DataToCSV(filtered_data, 'github_daily_data.csv', file_type='daily')
+        except (ValueError, IndexError, ConnectionError) as e:
+            logging.info(f"Error occurred during daily parsing: {str(e)}")
 
 
 class DataToCSV:
@@ -57,20 +65,26 @@ class DataToCSV:
             self.data, self.file_name)
 
     def projects_data_to_csv(self, data, file_name):
-        with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['Project Name', 'Total Project Commit Count'])
-            for i in data:
-                writer.writerow(i)
-        print(f'GitHub Projects Data saved as \'{file_name}\'')
+        try:
+            with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['Project Name', 'Total Project Commit Count'])
+                for i in data:
+                    writer.writerow(i)
+            print(f'GitHub Projects Data saved as \'{file_name}\'')
+        except IOError as e:
+            logging.info(f"Error occurred while trying to save projects data as CSV: {str(e)}")
 
     def daily_data_to_csv(self, data, file_name):
-        with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['Date', 'Commit Count'])
-            for i in data:
-                writer.writerow([' '.join(i[-1]), i[0][0]])
-        print(f'Daily GitHub Data saved as \'{file_name}\'')
+        try:
+            with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['Date', 'Commit Count'])
+                for i in data:
+                    writer.writerow([' '.join(i[-1]), i[0][0]])
+            print(f'Daily GitHub Data saved as \'{file_name}\'')
+        except IOError as e:
+            logging.info(f"Error occurred while trying to save daily data as CSV: {str(e)}")
 
 
 class GraphCSV:
@@ -79,13 +93,12 @@ class GraphCSV:
 
     def fetch_csv(self):
         csv_files = list(filter(lambda x: x.endswith('.csv'), os.listdir(self.path)))
-        return self.open_csv(csv_files)
+        return self.graph_csv(csv_files)
 
-    def open_csv(self, csv_files):
+    def graph_csv(self, csv_files):
         num_files = len(csv_files)
-
-        # Set the figure size and spacing
-        fig, axes = plt.subplots(nrows=num_files, figsize=(10, 6 * num_files), gridspec_kw={'hspace': 0.5})
+        
+        _, axes = plt.subplots(nrows=num_files, figsize=(10, 6 * num_files), gridspec_kw={'hspace': 0.5})
 
         for i, file in enumerate(csv_files):
             data = pd.read_csv(self.path / file, delimiter=',')
@@ -96,11 +109,7 @@ class GraphCSV:
 
             # Create separate subplot for each file
             ax = axes[i] if num_files > 1 else axes
-
-            # Plot the data with customized marker and line style
             ax.plot(x, y, marker='o', linestyle='-', label=file, color='green')
-
-            # Set title and axis labels for each subplot
             ax.set_title(file, fontsize=14)
             ax.set_xlabel(column1, fontsize=12)
             ax.set_ylabel(column2, fontsize=12)
@@ -108,28 +117,24 @@ class GraphCSV:
             # Set y-axis tick values as integers
             ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-        # Add legend for each subplot
         ax.legend(fontsize=10)
-
-        # Adjust layout and spacing
-        plt.tight_layout()
-
-        # Show all the plots
         plt.show()
 
 
 async def main():
-    print('Make sure to have your projects in the same directory as this script.')
-    github_user = input('Enter your GitHub username: ')
-    projects = list(filter(lambda x: x.isalpha(), os.listdir()))
-    async with ClientSession() as session:
-        github = GithubCommit(github_user, projects)
-        await asyncio.gather(
-            github.projects_parse_url(session),
-            github.daily_parse_url(session)
-        )
-    all_data = GraphCSV().fetch_csv()
-    print(all_data)
+    try:
+        github_user = input('Enter your GitHub username: ')
+        projects = list(filter(lambda x: x.isalpha(), os.listdir()))
+        async with ClientSession() as session:
+            github = GithubCommit(github_user, projects)
+            await asyncio.gather(
+                github.projects_parse_url(session),
+                github.daily_parse_url(session)
+            )
+        graphs = GraphCSV().fetch_csv()
+        print(graphs)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
