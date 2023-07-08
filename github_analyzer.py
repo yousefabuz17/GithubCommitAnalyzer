220433186ea9c8330053148d392f25120b915e1c
yousefabuz17 <yousef_abuzahrieh@aol.com>
2023-07-07 23:49:05 -0400

    Divided classes. Multithreading purposes

--- github_analyzer.py
+++ github_analyzer.py
@@ -1,10 +1,12 @@
 import requests
 import csv
 import os
+import asyncio
 from datetime import datetime as dt
 from rich.console import Console
 from pathlib import Path
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 
 
@@ -28,15 +30,7 @@ class GithubCommit:
                     commits_index = [m-1 for m,_ in enumerate(j) if _=='commits'][0]
                     commits_num = int(j[commits_index:commits_index + 2][0])
                     data.append((project_names, commits_num))
-        return self.projects_data_to_csv(data, 'github_projects_data.csv')
-    
-    def projects_data_to_csv(self, data, file_name):
-        with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['Project Name', 'Total Project Commit Count'])
-            for i in data:
-                writer.writerow(i)
-        return f'GitHub Projects Data saved as \'{file_name}\''
+        return DataToCSV(data, 'github_projects_data.csv', file_type='projects')
     
     def daily_parse_url(self):
         data = []
@@ -48,8 +42,23 @@ class GithubCommit:
                     continue
                 j = j.text.split()
                 data.append(([i if i.isdigit() else 0 for i in j[:3]],j[-3:]))
-        updated_data = list(filter(lambda x: str(x)!='([], [])', data))
-        return self.daily_data_to_csv(updated_data, 'github_daily_data.csv')
+        filtered_data = list(filter(lambda x: str(x)!='([], [])', data))
+        return DataToCSV(filtered_data, 'github_daily_data.csv', file_type='daily')
+
+class DataToCSV:
+    def __init__(self, data, file_name, file_type):
+        self.data = data
+        self.file_name = file_name
+        self.file_type = file_type
+        self.projects_data_to_csv(self.data, self.file_name) if self.file_type=='projects' else self.daily_data_to_csv(self.data, self.file_name)
+
+    def projects_data_to_csv(self, data, file_name):
+        with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Project Name', 'Total Project Commit Count'])
+            for i in data:
+                writer.writerow(i)
+        print(f'GitHub Projects Data saved as \'{file_name}\'')
     
     def daily_data_to_csv(self, data, file_name):
         with open(Path.cwd() / 'GithubCommitAnalyzer' / file_name, 'w', newline='') as f:
@@ -57,12 +66,12 @@ class GithubCommit:
             writer.writerow(['Date', 'Commit Count'])
             for i in data:
                 writer.writerow([' '.join(i[-1]), i[0][0]])
-        return f'Daily GitHub Data saved as \'{file_name}\''
+        print(f'Daily GitHub Data saved as \'{file_name}\'')
     
 def main():
     projects = list(filter(lambda x: x.isalpha(), os.listdir()))
     github = GithubCommit('yousefabuz17', projects)
-    github_daily = GithubCommit('yousefabuz17').daily_parse_url()
-    github_project = github.projects_parse_url()
+    github_daily_data = GithubCommit('yousefabuz17').daily_parse_url()
+    github_project_data = github.projects_parse_url()
 main()
 
