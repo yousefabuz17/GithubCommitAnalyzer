2572f2d16765f31f92823b7e99b39f512661a6b2
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-11 04:38:29 -0400

    streamlit_app created

---
+++ GitHubDashboard/streamlit_app.py
@@ -0,0 +1,32 @@
+import os
+import streamlit as st
+import streamlit.config as config
+
+from pathlib import Path
+from rich.console import Console
+
+console = Console()
+
+def streamlit_graphs():
+    st.set_page_config(
+        page_title="GitHub Dashboard",
+        page_icon="📊",
+        layout="centered",
+        initial_sidebar_state="expanded",
+    )
+    config.set_option("server.enableCORS", False)
+    config.set_option("global.developmentMode", False)
+
+    console.print("[bold green]\nStreamlit server is running on port 8501[/bold green]")
+    console.print('Press CTRL+C or close terminal anytime to terminate streamlit server', style='bold yellow')
+    console.print("[link]Please visit my GitHub repository to see all of my projects:\nhttps://github.com/yousefabuz17[/link]")
+    graph_path = Path(__file__).resolve().parent.parent / 'Figures'
+    graph_files = os.listdir(graph_path)
+    st.title("GitHub Dashboard")
+    for idx, graph in enumerate(graph_files, start=1):
+        st.header(f"Graph {idx}")
+        image_path = str(graph_path / graph)
+        st.image(image_path, use_column_width=True)
+
+if __name__ == '__main__':
+    streamlit_graphs()
