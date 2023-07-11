7d682e73e87fe98607cbb5a33f0243515c43d618
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-11 04:53:13 -0400

    Files in ascending order. Recent at top.

--- GitHubDashboard/streamlit_app.py
+++ GitHubDashboard/streamlit_app.py
@@ -12,7 +12,7 @@ def streamlit_graphs():
         page_title="GitHub Dashboard",
         page_icon="📊",
         layout="centered",
-        initial_sidebar_state="expanded",
+        initial_sidebar_state="auto",
     )
     config.set_option("server.enableCORS", False)
     config.set_option("global.developmentMode", False)
@@ -20,8 +20,11 @@ def streamlit_graphs():
     console.print("[bold green]\nStreamlit server is running on port 8501[/bold green]")
     console.print('Press CTRL+C or close terminal anytime to terminate streamlit server', style='bold yellow')
     console.print("[link]Please visit my GitHub repository to see all of my projects:\nhttps://github.com/yousefabuz17[/link]")
+    
     graph_path = Path(__file__).resolve().parent.parent / 'Figures'
-    graph_files = os.listdir(graph_path)
+    graph_files = sorted(os.listdir(graph_path), key=lambda x: os.path.getmtime(os.path.join(graph_path, x)), reverse=True)
+
+    console.print(f"\nThere are a total of {len(graph_files)} graphs being displayed on the dashboard", style='bold white')
     st.title("GitHub Dashboard")
     for idx, graph in enumerate(graph_files, start=1):
         st.header(f"Graph {idx}")
